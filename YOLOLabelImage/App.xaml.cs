06000dfc8d5dfb815311e76bc61dcb2f50b6aa65
Windows.h <1143910315@qq.com>
2023-10-16 18:13:14 +0800

    添加项目文件。

---
+++ YOLOLabelImage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YOLOLabelImage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ YOLOLabelImage/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace YOLOLabelImage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
