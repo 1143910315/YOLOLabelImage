dd2196deb4418bec3c7b8bf268f2cf88c2cfdd9e
Windows.h <1143910315@qq.com>
2023-10-16 21:33:01 +0800

    设置数据视图模型

---
+++ YOLOLabelImage/ViewModel/SettingViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOLOLabelImage.ViewModel {
+    internal class SettingViewModel : ViewModelBase {
+    }
+}
---
+++ YOLOLabelImage/ViewModel/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOLOLabelImage.ViewModel {
+    internal class ViewModelBase {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null) {
+            if (EqualityComparer<T>.Default.Equals(field, newValue)) {
+                return false;
+            }
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        protected bool Update([CallerMemberName] string? propertyName = null) {
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
